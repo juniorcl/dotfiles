a12fb5fc0fc1cc29276c3a86f040d35376d60258
Júnior <clebiojuniorcj@gmail.com>
2017-09-07 07:35:23 -0300

    New update with more adjustments

--- dzen2/dzenbar.sh
+++ dzen2/dzenbar.sh
@@ -10,51 +10,74 @@
 ## Dzen Config
 ##############
 ##############
-#ICON_PATH=
-#ICON_COLOR=
-DZEN_BG="#000000"
+ICON_PATH="$HOME/.dzen2/icon"
+DZEN_BG="#770000"
 DZEN_FG="#BEBEBE"
-SEPARATOR="^fg(red).:^fg()"
+SEPARATOR="^fg(red)|^fg()"
 DZEN_FONT="-*-terminus-medium-r-*-*-12-*-*-*-*-*-iso10646-*"
 SLEEP=2
+X=713
+Y=0
+WIDTH=650
 
-DZEN="dzen2 -ta r -fn $DZEN_FONT -bg $DZEN_BG -fg $DZEN_FG -dock"
+DZEN="dzen2 -ta r -fn $DZEN_FONT -bg $DZEN_BG -fg $DZEN_FG -x $X -y $Y -w $WIDTH"
 
 ## Dzen2 Info
 #############
 #############
+function Midle()
+{
+    echo -n "^fg($DZEN_FG)^i($ICON_PATH/da-024-l.xbm) "
+}
+
 function Kernel()
 {
     kernel=$(uname -a | awk '{print $3}')
-    echo -n "^fg($DZEN_FG) $kernel ^fg() $SEPARATOR"
+    echo -n "^i($ICON_PATH/Arch.xbm)^fg($DZEN_FG) $kernel ^fg() $SEPARATOR"
     return
 }
 
 function Root()
 {
     root_free=$(df -h / | sed '1d' | awk '{print $4}')
-    echo -n "^fg($DZEN_FG) $root_free ^fg() $SEPARATOR" 
+    #root_free_porcent=$(df -h /home | sed '1d' | awk '{print $5}')
+    echo -n "^fg($DZEN_FG) $root_free $root_free_porcent ^fg() $SEPARATOR" 
     return
 }
 
 function Home()
 {
     home_free=$(df -h /home | sed '1d' | awk '{print $4}')
-    echo -n "^fg($DZEN_FG) $home_free ^fg() $SEPARATOR" 
+    #home_free_porcent=$(df -h /home | sed '1d' | awk '{print $5}')
+    echo -n "^fg($DZEN_FG) $home_free $home_free_porcent ^fg() $SEPARATOR" 
     return
 }
 
 function Ram()
 {
     ram=$(free -mh | awk '{print $3}' | sed '1d' | sed '2d')
-    echo -n "^fg($DZEN_FG) $ram ^fg() $SEPARATOR"
+    ram_porcent=$(free | grep Mem | awk '{print $3/$2 * 100}')
+    echo -n " ^i($ICON_PATH/Mem.xbm)^fg($DZEN_FG) $ram $ram_porcent% ^fg() $SEPARATOR"
     return
 }
 
 function Swap()
 {
     swap=$(free -mh | awk '{print $3}' | sed '1d' | sed '1d')
-    echo -n "^fg($DZEN_FG) $swap ^fg() $SEPARATOR"
+    swap_porcent=$(free | grep Swap | awk '{print $3/$2 * 100}')
+    echo -n "^fg($DZEN_FG) $swap $swap_porcent% ^fg() $SEPARATOR"
+    return
+}
+
+function Wifi()
+{
+    wifi_name=$(netctl list)
+    if test -n $wifi_name
+    then
+        echo -n "$wifi_name $SEPARATOR"
+    else
+        echo -n "Not Connected $SEPARATOR"
+    fi
     return
 }
 
@@ -77,11 +100,13 @@ function Time()
 ##############
 Print()
 {
+    #Midle
     Kernel
     Root
     Home
     Ram
     Swap
+    Wifi
     Date
     Time
     return
