4723348acd7c414a96ff1e34fe5c9028e278568a
Clébio Júnior <clebiojuniorcj@gmail.com>
2020-06-26 08:54:36 -0300

    created manjaro folder

---
+++ manjaro/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+
+echo "################################################"
+echo "##         Setup Manajaro Installer           ##"
+echo "################################################"
+echo
+echo
+
+## Insta
