6c185a92e0fdc0dad2bcf96b1d1d480a2a83ec4a
Clébio Júnior <clebiojuniorcj@gmail.com>
2020-06-26 09:21:51 -0300

    setup manjato file

---
+++ archlinux/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clear
+
+echo "################################################"
+echo "##         Setup Manajaro Installer           ##"
+echo "################################################"
+echo
+echo
+
+## Pyenv Install
+################
+echo "Installing Pyenv.."
--- manjaro/setup.sh
+++ manjaro/setup.sh
@@ -7,4 +7,31 @@ echo "################################################"
 echo
 echo
 
-## Insta
+## Changing Bash to ZSH
+#######################
+echo "Changing Bash to ZSH.."
+cp manjaro/zsh/.zshrc ~/.zshrc
+chsh -l /usr/bin/zsh
+
+## Pyenv Install
+################
+echo "Installing Pyenv.."
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+#instalation for zsh shell
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+
+## Intalling yay
+################
+echo "Installing yay.."
+git clone https://aur.archlinux.org/yay.git ~/.yay
+cd .yay
+makepkg -si
+
+## Installing vscode
+####################
+echo "Installing vscode.."
+yay -S visual-studio-code-bin
+
