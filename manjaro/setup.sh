1043c747fe7e72e62c63da9f56c8a617018d4a60
Clébio Júnior <clebiojuniorcj@gmail.com>
2020-06-28 20:05:11 -0300

    colorful configuration implemented

--- manjaro/setup.sh
+++ manjaro/setup.sh
@@ -1,51 +1,52 @@
 #!/bin/bash
 clear
 
-
-echo "################################################"
-echo "################################################"
-echo "##                                            ##"
-echo "##          Manajaro Installer Setup          ##"
-echo "##                                            ##"
-echo "################################################"
-echo "################################################"
-echo
-echo
-sleep 1
-
 ## Preprocessing
 ################
-echo "Starting preprocessing.."
-sleep 1
-sudo pacman -Syu
-sudo pacman -S base-devel
+cho -e "\e[1;35m==>\e[0m \e[1mGeneral Instalation\e[0m"
+echo -e " \e[93m->\e[0m \e[1mInstaling base-devel..\e[0m"
+sudo pacman -S --noconfirm base-devel
 echo
 
-## Changing Bash to ZSH
-#######################
-echo "Changing Bash to ZSH.."
-sleep 1
+## Instaling ZSH and setting up ZSH
+###################################
+echo -e "\e[1;35m==>\e[0m \e[1mZsh\e[0m"
+echo -e " \e[93m->\e[0m \e[1mInstaling Zsh..\e[0m"
+sudo pacman -S --noconfirm zsh
+echo
+
+echo -e " \e[93m->\e[0m \e[1mCopying .zshrc to ~/.zshrc..\e[0m"
 cp $DOTFILES/manjaro/zsh/.zshrc ~/.zshrc
+echo
+
+echo -e " \e[93m->\e[0m \e[1mSetting Zsh as default..\e[0m"
 chsh -l /usr/bin/zsh
 echo
 
 ## Pyenv Install
 ################
-echo "Instaling Pyenv.."
-sleep 1
+echo -e "\e[1;35m==>\e[0m \e[1mPyenv\e[0m"
+echo -e " \e[93m->\e[0m \e[1mClonando pyenv..\e[0m"
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo
 
-#instalation for zsh shell
+echo -e " \e[93m->\e[0m \e[1mDefine environment variable PYENV_ROOT..\e[0m"
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo
+
+echo -e " \e[93m->\e[0m \e[1mAdd pyenv init to your shell..\e[0m"
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 echo
 
 ## Intalling yay
 ################
-echo "Instaling yay.."
-sleep 1
+echo -e "\e[1;35m==>\e[0m \e[1mYay\e[0m"
+echo -e " \e[93m->\e[0m \e[1mCloning yay..\e[0m"
 git clone https://aur.archlinux.org/yay.git ~/.yay
+echo
+
+echo -e " \e[93m->\e[0m \e[1mInstaling yay..\e[0m"
 cd ~/.yay
 makepkg -si 
 cd ~/
@@ -53,68 +54,73 @@ echo
 
 ## Instaling vscode
 ####################
-echo "Instaling vscode.."
-sleep 1
+echo -e "\e[1;35m==>\e[0m \e[1mVscode\e[0m"
+echo -e " \e[93m->\e[0m \e[1mInstaling vscode..\e[0m"
 yay -S visual-studio-code-bin
 echo
 
 ## Instaling Firefox in pt-br
 ##############################
-echo "Instaling Firefox in Brazilian Portuguese.."
-sleep 1
-sudo pacman -S --noconfirm firefox firefox-i18n-pt-br
+echo -e "\e[1;35m==>\e[0m \e[1mFirefox\e[0m"
+echo -e " \e[93m->\e[0m \e[1mInstaling firefox..\e[0m"
+sudo pacman -S --noconfirm firefox
+echo
+
+echo -e " \e[93m->\e[0m \e[1mInstaling Firefox in Brazilian Portuguese..\e[0m"
+sudo pacman -S firefox-i18n-pt-br
 echo
 
 ## Instaling hack font
 #######################
-echo "Instaling Hack Font.."
-sleep 1
+echo -e "\e[1;35m==>\e[0m \e[1mHack Font\e[0m"
+echo -e " \e[93m->\e[0m \e[1mInstaling hack font..\e[0m"
 sudo pacman -S --noconfirm ttf-hack
 echo
 
 ## UnInstaling Midori Brownser
 ###############################
-echo "UnInstaling midori.."
-sleep 1
+echo -e "\e[1;35m==>\e[0m \e[1mMidori\e[0m"
 
 if [ -e "$(which midori 2> /dev/null)" ]
 then
-    echo "UnInstaling midori Browser.."
-    echo
+    echo -e " \e[93m->\e[0m \e[1mUninstaling midori Browser..\e[0m"
     sudo pacman -Rs --noconfirm midori
-    echo "Midori Uninstalled!"
-	sleep 1
-
+    echo -e " \e[93m->\e[0m \e[1mMidori Uninstalled!\e[0m"
+	
 else
-    echo "Midori ins't installed!"
-	sleep 1
+    echo -e " \e[93m->\e[0m \e[1mMidori is not installed!\e[0m"
+
 fi
 echo
 
 ## Instaling qBitorrent
 ########################
-echo "Instaling qBittorrent.."
-sleep 1
-sudo pacman -S --noconfirm qbittorrent
+echo -e "\e[1;35m==>\e[0m \e[1mqBitorrent\e[0m"
+echo -e " \e[93m->\e[0m \e[1mInstaling qBittorrent..\e[0m"
+sudo pacman -S --noconfirm qBittorrent
 echo
 
 ## Install Materia Theme
 ########################
-echo "Instaling Materia Theme.."
-sleep 1
-yay -S --noconfirm materia-gtk-theme
+echo -e "\e[1;35m==>\e[0m \e[1mMateria Theme\e[0m"
+echo -e " \e[93m->\e[0m \e[1mInstaling Materia Theme..\e[0m"
+yay -S materia-gtk-theme
 echo
 
 ## Install R
 ############
-echo "Instaling R language.."
-sleep 1
+echo -e "\e[1;35m==>\e[0m \e[1mR Language\e[0m"
+echo -e " \e[93m->\e[0m \e[1mInstaling R..\e[0m"
 sudo pacman -S --noconfirm r
+echo
+
+echo -e " \e[93m->\e[0m \e[1mInstaling RStudio..\e[0m"
 yay -S --noconfirm rstudio-desktop-bin
 echo
 
 ## Install VLC
 ##############
-echo "Instaling VLC.."
-sleep 1
+echo -e "\e[1;35m==>\e[0m \e[1mVLC\e[0m"
+echo -e " \e[93m->\e[0m \e[1mInstaling vlc..\e[0m"
 sudo pacman -S vlc
+echo
