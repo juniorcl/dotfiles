c8a6046b34060b78818a4427b0be2e7829e699d8
Clébio Júnior <clebiojuniorcj@gmail.com>
2020-06-30 19:38:19 -0300

    replaced yellowArrows by subTopic

--- manjaro/setup.sh
+++ manjaro/setup.sh
@@ -6,7 +6,7 @@ Topic()
     echo -e "\e[1;35m::\e[0m \e[1m$1\e[0m"
 }
 
-yellowArrows()
+subTopic()
 {
     echo -e "\e[1;93m >>\e[0m \e[1m$1\e[0m"
 }
@@ -14,49 +14,49 @@ yellowArrows()
 ## Preprocessing
 ################
 Topic "General Instalations"
-yellowArrows "Instaling base-devel.."
+subTopic "Instaling base-devel.."
 sudo pacman -S --noconfirm base-devel
 echo
 
 ## Instaling ZSH and setting up ZSH
 ###################################
 Topic "Zsh"
-yellowArrows "Instaling Zsh.."
+subTopic "Instaling Zsh.."
 sudo pacman -S --noconfirm zsh
 echo
 
-yellowArrows "Copying .zshrc to ~/.zshrc.."
+subTopic "Copying .zshrc to ~/.zshrc.."
 cp $DOTFILES/manjaro/zsh/.zshrc ~/.zshrc
 echo
 
-yellowArrows "Setting Zsh as default.."
+subTopic "Setting Zsh as default.."
 chsh -l /usr/bin/zsh
 echo
 
 ## Pyenv Install
 ################
 Topic "Pyenv"
-yellowArrows "Clonando pyenv.."
+subTopic "Clonando pyenv.."
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo
 
-yellowArrows "Define environment variable PYENV_ROOT.."
+subTopic "Define environment variable PYENV_ROOT.."
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo
 
-yellowArrows "Add pyenv init to your shell.."
+subTopic "Add pyenv init to your shell.."
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 echo
 
 ## Intalling yay
 ################
 Topic "Yay"
-yellowArrows "Cloning yay.."
+subTopic "Cloning yay.."
 git clone https://aur.archlinux.org/yay.git ~/.yay
 echo
 
-yellowArrows "Instaling yay.."
+subTopic "Instaling yay.."
 cd ~/.yay
 makepkg -si 
 cd ~/
@@ -65,25 +65,25 @@ echo
 ## Instaling vscode
 ####################
 Topic "Visual Studio Code"
-yellowArrows "Instaling vscode.."
+subTopic "Instaling vscode.."
 yay -S visual-studio-code-bin
 echo
 
 ## Instaling Firefox in pt-br
 ##############################
 Topic "Firefox"
-yellowArrows "Instaling firefox.."
+subTopic "Instaling firefox.."
 sudo pacman -S --noconfirm firefox
 echo
 
-yellowArrows "Instaling Firefox in Brazilian Portuguese.."
+subTopic "Instaling Firefox in Brazilian Portuguese.."
 sudo pacman -S firefox-i18n-pt-br
 echo
 
 ## Instaling hack font
 #######################
 Topic "Hack Font"
-yellowArrows "Instaling hack font.."
+subTopic "Instaling hack font.."
 sudo pacman -S --noconfirm ttf-hack
 echo
 
@@ -93,12 +93,12 @@ Topic "Midori"
 
 if [ -e "$(which midori 2> /dev/null)" ]
 then
-    yellowArrows "Uninstaling midori Browser.."
+    subTopic "Uninstaling midori Browser.."
     sudo pacman -Rs --noconfirm midori
-    yellowArrows "Midori Uninstalled!"
+    subTopic "Midori Uninstalled!"
 	
 else
-    yellowArrows "Midori is not installed!"
+    subTopic "Midori is not installed!"
 
 fi
 echo
@@ -109,12 +109,12 @@ Topic "Parole"
 
 if [ -e "$(which parole 2> /dev/null)" ]
 then
-    yellowArrows "Uninstaling Parole Media Player.."
+    subTopic "Uninstaling Parole Media Player.."
     sudo pacman -Rs --noconfirm midori
-    yellowArrows "Parole Uninstalled!"
+    subTopic "Parole Uninstalled!"
 	
 else
-    yellowArrows "Parole is not installed!"
+    subTopic "Parole is not installed!"
 
 fi
 echo
@@ -122,59 +122,59 @@ echo
 ## Instaling qBitorrent
 ########################
 Topic "qBitorrent"
-yellowArrows "Instaling qBittorrent.."
+subTopic "Instaling qBittorrent.."
 sudo pacman -S --noconfirm qbittorrent
 echo
 
 ## Install Materia Theme
 ########################
 Topic "Materia Theme"
-yellowArrows "Instaling Materia Theme.."
+subTopic "Instaling Materia Theme.."
 yay -S materia-gtk-theme
 echo
 
 ## Install R
 ############
 Topic "R Language"
-yellowArrows "Instaling R.."
+subTopic "Instaling R.."
 sudo pacman -S --noconfirm r
 echo
 
-yellowArrows "Instaling RStudio.."
+subTopic "Instaling RStudio.."
 yay -S --noconfirm rstudio-desktop-bin
 echo
 
 ## Install VLC
 ##############
 Topic "VLC Media Player"
-yellowArrows "Instaling vlc.."
+subTopic "Instaling vlc.."
 sudo pacman -S vlc
 echo
 
 ## Install ferdi
 ################
 Topic "Ferdi"
-yellowArrows "Instaling Ferdi.."
+subTopic "Instaling Ferdi.."
 yay -S ferdi-bin
 echo
 
 ## Install Telegram desktop
 ###########################
 Topic "Telegram Desktop"
-yellowArrows "Instaling Telegram.."
+subTopic "Instaling Telegram.."
 sudo pacman -S --noconfirm telegram-desktop
 echo
 
 ## Install Keepassxc
 ####################
 Topic "Keepassxc"
-yellowArrows "Instaling Keepassxc.."
+subTopic "Instaling Keepassxc.."
 sudo pacman -S --noconfirm Keepassxc
 echo
 
 ## Install Dropbox
 ##################
 Topic "Dropbox"
-yellowArrows "Instaling Dropbox.."
+subTopic "Instaling Dropbox.."
 yay -S dropbox
 echo
