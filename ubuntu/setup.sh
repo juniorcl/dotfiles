840fd4b51a763d7d49876aee852e58c4f00160c5
Clébio Júnior <clebiojuniorcj@gmail.com>
2020-07-07 13:15:43 -0300

    pyenv and r added

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -31,3 +31,34 @@ Topic "VLC"
 subTopic "Instaling VLC.."
 sudo snap install vlc
 echo
+
+## Instalation Pyenv
+####################
+Topic "Pyenv"
+subTopic "Clonando pyenv.."
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo
+
+subTopic "Define environment variable PYENV_ROOT.."
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo
+
+subTopic "Add pyenv init to your shell.."
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+echo
+
+## Instalation R
+################
+Topic "R"
+subTopic "instaling R.."
+sudo apt install r-base
+echo
+
+subTopic "Downloading RStudio.."
+sudo wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.3.959-amd64.deb
+echo
+
+subTopic "Instaling RStudio.."
+sudo dpkg -i rstudio*.deb
+echo
