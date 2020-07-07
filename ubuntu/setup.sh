13c0608905cc24b86442942c425238793b467e50
Clébio Júnior <clebiojuniorcj@gmail.com>
2020-07-07 12:22:48 -0300

    vscode added and other some fixing

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -11,7 +11,13 @@ subTopic()
 	    echo -e "\e[1;93m >>\e[0m \e[1m$1\e[0m"
 }
 
-## Instalação Vscode
+## Instalation Vim
+##################
+Topic "Vim"
+subTopic "Instaling Vim"
+sudo apt install vim
+
+## Instalation Vscode
 ####################
 Topic "VScode"
 subTopic "Instaling VScode.."
