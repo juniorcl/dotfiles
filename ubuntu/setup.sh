0fcadd9c708925e45893a093ff2a116d955d168b
Clébio de Oliveira Júnior <clebiojuniorcj@gmail.com>
2021-08-31 12:31:13 -0300

    Update pyenv install

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -35,17 +35,18 @@ echo
 ## Instalation Pyenv
 ####################
 Topic "Pyenv"
-subTopic "Clonando pyenv.."
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+subTopic "Installing Dependencies"
+sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 echo
 
-subTopic "Define environment variable PYENV_ROOT.."
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+subTopic "Installing"
+curl https://pyenv.run | bash
 echo
 
-subTopic "Add pyenv init to your shell.."
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+subTopic "Restating the Shell"
+exec $SHELL
 echo
 
 ## Instalation KeePassXC
