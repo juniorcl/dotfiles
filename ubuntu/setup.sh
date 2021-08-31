3438773fb15096807443f1cf92e0fd81c8981452
Clébio de Oliveira Júnior <clebiojuniorcj@gmail.com>
2021-08-31 13:30:51 -0300

    Updating pyenv install

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -45,6 +45,22 @@ subTopic "Installing"
 curl https://pyenv.run | bash
 echo
 
+subTopic "Adding Commands"
+echo -e 'if shopt -q login_shell; then' \
+      '\n  export PYENV_ROOT="$HOME/.pyenv"' \
+      '\n  export PATH="$PYENV_ROOT/bin:$PATH"' \
+      '\n eval "$(pyenv init --path)"' \
+      '\nfi' >> ~/.bashrc
+
+echo -e 'if [ -z "$BASH_VERSION" ]; then'\
+      '\n  export PYENV_ROOT="$HOME/.pyenv"'\
+      '\n  export PATH="$PYENV_ROOT/bin:$PATH"'\
+      '\n  eval "$(pyenv init --path)"'\
+      '\nfi' >> ~/.profile
+
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc 
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
 subTopic "Restating the Shell"
 exec $SHELL
 echo
